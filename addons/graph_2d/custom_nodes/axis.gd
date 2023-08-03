5c9ba384f89c38072aa650c77299923025ea242b
Radhe <30527877+Radhe-0@users.noreply.github.com>
2023-08-03 07:15:37 -0500

    Update axis.gd

--- addons/graph_2d/custom_nodes/axis.gd
+++ addons/graph_2d/custom_nodes/axis.gd
@@ -12,6 +12,13 @@ var vert_grad: Array # [Vector2, String]
 var hor_grad: Array
 var x_label: String
 var y_label: String
+var show_x_ticks: bool
+var show_y_ticks: bool
+var show_x_numbers: bool
+var show_y_numbers: bool
+var show_vertical_line: bool
+var show_horizontal_line: bool
+
 
 func _ready():
 	name = "Axis"
@@ -32,19 +39,29 @@ func _draw() -> void:
 	var topright: Vector2 = Vector2(hor_grad.back()[POINT].x, vert_grad.front()[POINT].y)
 	var bottomright: Vector2 = hor_grad.back()[POINT]
 	
-	for grad in vert_grad:
-		draw_line(grad[POINT], grad[POINT] - Vector2(10, 0), Color.WHITE)
-		draw_string(default_font, grad[0] + Vector2(-35, -5), grad[1])
-	# draw vertical line
-	draw_line(topleft, vert_grad.back()[POINT], Color.WHITE)
-	get_node("YLabel").text = y_label
-	get_node("YLabel").position = Vector2(5, (bottomright.y + topleft.y)/2)
-		
-	for grad in hor_grad:
-		draw_line(grad[POINT], grad[POINT] + Vector2(0, 10), Color.WHITE)
-		draw_string(default_font, grad[POINT] + Vector2(0, 20), grad[LABEL])
+	if show_x_ticks:
+		for grad in hor_grad: 
+			draw_line(grad[POINT], grad[POINT] + Vector2(0, 10), Color.WHITE)
+	
+	if show_x_numbers:
+		for grad in hor_grad:
+			draw_string(default_font, grad[POINT] + Vector2(0, 20), grad[LABEL])
+	
+	if show_horizontal_line == true:
+		draw_line(hor_grad.front()[POINT], hor_grad.back()[POINT], Color.WHITE)
 
-	# draw horizontal line
-	draw_line(hor_grad.front()[POINT], hor_grad.back()[POINT], Color.WHITE)
+	if show_y_ticks == true:
+		for grad in vert_grad:
+			draw_line(grad[POINT], grad[POINT] - Vector2(10, 0), Color.WHITE)
+	
+	if show_y_numbers == true:
+		for grad in vert_grad:
+			draw_string(default_font, grad[0] + Vector2(-35, -5), grad[1])
+		
+	if show_vertical_line == true:
+		draw_line(topleft, vert_grad.back()[POINT], Color.WHITE)
+
 	get_node("XLabel").text = x_label
+	get_node("YLabel").text = y_label 
 	get_node("XLabel").position = Vector2((bottomright.x + topleft.x)/2, bottomright.y + 20)
+	get_node("YLabel").position = Vector2(5, (bottomright.y + topleft.y)/2)
